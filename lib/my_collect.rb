46578e0cc97f2250f13276cb9e147e574a20071e
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-05 20:33:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@
 
 def my_collect(collection)
   empty_array = []
-  my_collect(languages) do |language|
+  my_collect(languages).collect do |language|
     puts empty_array << language.upcase
   end
 end
